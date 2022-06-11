139b6e3a275dbf67b84fe0133a39fe3ebb35b15a
atharvagangrade10 <44475067+atharvagangrade10@users.noreply.github.com>
2022-06-11 16:34:10 +0530

    CH4 Assignment 2
    Tank Spawner Scripts and Prefab Creation

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    public GameObject tankPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnTank();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnTank()
+    {
+        Instantiate(tankPrefab, gameObject.transform.position, Quaternion.identity);
+    }
+}
